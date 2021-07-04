7dfd7c324abc8489a56ace4648cf9a6d3f121529
N Priyajit <47923239+NPriyajit@users.noreply.github.com>
2021-07-04 19:41:01 +0530

    Add Register functionality

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 const express=require('express');
 const app=express();
 const mysql = require('mysql2');
-
-
-// const conn = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "root",
-//     insecureAuth : true,
-//     database: "port"
-//   });
+const bodyParser=require('body-parser');
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    insecureAuth : true,
+    database: "spectrum"
+  });
   
-// conn.connect((err)=>{
-// if (err) throw err;
-// console.log("DB Connected!");
-// });
+conn.connect((err)=>{
+if (err) throw err;
+console.log("DB Connected!");
 
-// conn.query("SELECT * FROM user", function (err, result) {
-//     if (err) throw err;
-//     console.log(result);
-// });
-
-// conn.end();
+});
 
 
+
+
+let login=false;
+let msg="";
+
+app.use(express.urlencoded({ extended: true }));
 // EJS
 app.set('view engine', 'ejs');
 
@@ -32,16 +31,29 @@ app.use(express.static('./public'));
 
 
 app.get('/',(req,res)=>{
-     res.render('index',{login:true})
+     res.render('index',{login})
 })
 app.get('/login',(req,res)=>{
-     res.render('index')
+     res.render('login')
 })
 app.get('/register',(req,res)=>{
-     res.render('index')
+     res.render('register')
 })
 app.get('/logout',(req,res)=>{
-     res.render('index')
+    login=false;
+    res.redirect('/')
+})
+app.post('/login',(req,res)=>{
+    login=true;
+    res.redirect('/')
+})
+app.post('/register',(req,res)=>{
+    const {name, email, phno, branch, dob, password}=req.body;
+    var sql = "INSERT INTO `user` (`name`, `email`, `phno`, `branch`, `dob`, `password`) VALUES ('"+name+"','"+ email+"','"+ phno+"','"+ branch+"','"+ dob+"','"+ password+"')";
+    conn.query(sql, function (err, result) {
+        if (err) throw err;
+       
+    });
 })
 
 
